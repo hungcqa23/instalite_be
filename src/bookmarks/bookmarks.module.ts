8156072b92c7d61456dfb078190d4cc8af17df0f
hungcqa23 <anbeel191@gmail.com>
2024-04-26 20:08:49 +0700

    fix(files_s): fix error

--- src/bookmarks/bookmarks.module.ts
+++ src/bookmarks/bookmarks.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BookmarksController } from './bookmarks.controller';
 import { BookmarksService } from './bookmarks.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookMarkSchema } from 'src/bookmarks/bookmarks.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'BookMark', schema: BookMarkSchema }])],
   controllers: [BookmarksController],
-  providers: [BookmarksService]
+  providers: [BookmarksService],
+  exports: [BookmarksService]
 })
 export class BookmarksModule {}
--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LikesService } from './likes.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LikeSchema } from 'src/likes/like.schema';
 
 @Module({
-  providers: [LikesService]
+  imports: [MongooseModule.forFeature([{ name: 'Like', schema: LikeSchema }])],
+  providers: [LikesService],
+  exports: [LikesService]
 })
 export class LikesModule {}
